1d74e4bc816d09a350b31080256bc98bfd14bd3b
unknown <nongjossss@gmail.com>
2025-09-30 01:41:57 +0700

    Prepare codebase for final fix task

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <title>@yield('title', 'Company Records')</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
