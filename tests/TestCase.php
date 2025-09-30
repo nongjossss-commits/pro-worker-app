5d8ce6fe2fbbee07b22a376177dd03e41e6f6355
unknown <nongjossss@gmail.com>
2025-09-30 23:56:15 +0700

    Final Fix: Manually override TestCase.php to resolve autoloading issue

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,22 @@
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\CreatesApplication;
+use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication, LazilyRefreshDatabase;
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
